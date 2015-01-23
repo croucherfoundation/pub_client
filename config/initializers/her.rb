26852b8c13395068b6d712c1ce5133d20f75a295
William Ross <will@spanner.org>
2015-01-23 15:27:14 +0000

    no caching

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,30 +1,13 @@
 require 'settings'
 require 'paginated_her'
-require 'dalli-elasticache'
-require 'faraday_middleware/response/caching'
 
 Settings.pub ||= {}
 Settings.pub[:protocol] ||= 'http'
 Settings.pub[:api_host] ||= Settings.pub[:host] || 'localhost'
 Settings.pub[:api_port] ||= Settings.pub[:port] || 8007
 
-Settings[:memcached] ||= {}
-Settings.memcached[:endpoint] ||= nil
-Settings.memcached[:host] ||= nil
-Settings.memcached[:port] ||= 11211
-Settings.memcached[:ttl] ||= 10.minutes
-
-if Settings.memcached.endpoint
-  elasticache_endpoint = Dalli::ElastiCache.new("#{Settings.memcached.endpoint}:#{Settings.memcached.port}", expires_in: Settings.memcached.ttl)
-  $cache ||= elasticache_endpoint.client
-
-elsif Settings.memcached.host
-  $cache ||= Dalli::Client.new("#{Settings.memcached.host}:#{Settings.memcached.port}", expires_in: Settings.memcached.ttl)
-end
-
 PUB = Her::API.new
 PUB.setup url: "#{Settings.pub.protocol}://#{Settings.pub.api_host}:#{Settings.pub.api_port}" do |c|
-  c.use FaradayMiddleware::Caching, $cache.clone if $cache
   c.use Faraday::Request::UrlEncoded
   c.use PaginatedHer::Middleware::Parser
   c.use Faraday::Adapter::NetHttp
--- pub_client.gemspec
+++ pub_client.gemspec
@@ -18,8 +18,6 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.0"
   s.add_dependency 'paginated_her'
   s.add_dependency "faraday"
-  s.add_dependency "faraday_middleware"
-  s.add_dependency "dalli-elasticache"
   s.add_dependency "signed_json"
 
   s.add_development_dependency "sqlite3"
