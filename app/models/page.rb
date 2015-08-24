8f6dd3a45844d1393025e9867bd6fb2bde679d1e
William Ross <will@spanner.org>
2015-08-25 00:12:00 +0100

    more dates

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,8 @@ class Page
 
   use_api PUB
   collection_path "/api/pages"
+  has_many :images
+  belongs_to :page_collection
 
   # temporary while we are not yet sending jsonapi data back to core properly
   include_root_in_json true
@@ -12,10 +14,29 @@ class Page
     page = Page.new({
       project_id: nil,
       collection_id: nil,
+      published_at: nil,
+      featured_at: nil,
+      featured: false,
       name: "",
       slug: "",
     }.merge(attributes))
     page
+  end
+
+  def published?
+    published_at.present?
+  end
+
+  def published_date
+    DateTime.parse(published_at).in_time_zone(Rails.application.config.time_zone) if published_at.present?
+  end
+
+  def featured?
+    featured_at.present?
+  end
+
+  def featured_date
+    DateTime.parse(featured_at).in_time_zone(Rails.application.config.time_zone) if featured_at.present?
   end
 
 end
--- app/models/page_collection.rb
+++ app/models/page_collection.rb
@@ -4,4 +4,15 @@ class PageCollection
   use_api PUB
   collection_path "/api/page_collections"
 
+  # temporary while we are not yet sending jsonapi data back to core properly
+  include_root_in_json true
+  parse_root_in_json false
+
+  def self.new_with_defaults(attributes={})
+    page = Page.new({
+      title: "",
+      slug: ""
+    }.merge(attributes))
+    page
+  end
 end
