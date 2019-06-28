f4f39d8743a51f975851df12890766b522f920d4
will-r <will@spanner.org>
2019-06-28 10:58:48 +0100

    env variable for api base

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -2,13 +2,10 @@ require 'settings'
 require 'her'
 require 'faraday_middleware'
 
-Settings.pub ||= {}
-Settings.pub[:protocol] ||= 'http'
-Settings.pub[:api_host] ||= Settings.pub[:host] || 'localhost'
-Settings.pub[:api_port] ||= Settings.pub[:port] || 8007
+api_url = ENV['PUB_URL'] || "#{Settings.pub.protocol}://#{Settings.pub.api_host}:#{Settings.pub.api_port}"
 
 PUB = Her::API.new
-PUB.setup url: "#{Settings.pub.protocol}://#{Settings.pub.api_host}:#{Settings.pub.api_port}" do |c|
+PUB.setup url: api_url do |c|
   # Request
   c.use FaradayMiddleware::EncodeJson
   # Response
